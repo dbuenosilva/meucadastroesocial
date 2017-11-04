f55187e39334afbd44cb4d14c51d785907dfbf66
Diego <diego@bueno.eti.br>
2017-11-04 15:24:18 -0200

    main

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -7,56 +7,6 @@ window.onload = function () {
 };
 
 
-$(document).ready(function() {
-	$('#rootwizard').bootstrapWizard({
-		tabClass:"nav-wizard",
-		onTabShow: function(tab, navigation, index) {
-			tab.prevAll().addClass('completed');
-			tab.nextAll().removeClass('completed');
-			if(tab.hasClass("active")){
-				tab.removeClass('completed');
-			}
-			var $total = navigation.find('li').length;
-			var $current = index+1;
-			var $percent = ($current/$total) * 100;
-			$('#rootwizard').find('.progress-bar').css({width:$percent+'%'});
-			$('#rootwizard').find('.wizard-status span').html($current+" de "+$total);
-		}
-	});
-
-	$('#validate-wizard').bootstrapWizard({
-		tabClass:"nav-wizard",
-		onNext: function(tab, navigation, index) {
-			var content=$('#step'+index);
-			if(typeof  content.attr("parsley-validate") != 'undefined'){
-				var $valid = content.parsley( 'validate' );
-				if(!$valid){
-					return false;
-				}
-			};
-			// Set the name for the next tab
-			$('#step8 h3').find("span").html($('#fullname').val());
-		},
-		onTabClick: function(tab, navigation, index) {
-			$.notific8('Clique no botão <strong>Próximo</strong> no final da página para continuar!',{ life:5000, theme:"danger" ,heading:" Ops! "});
-			return false;
-		},
-		onTabShow: function(tab, navigation, index) {
-			tab.prevAll().addClass('completed');
-			tab.nextAll().removeClass('completed');
-			if(tab.hasClass("active")){
-				tab.removeClass('completed');
-			}
-			var $total = navigation.find('li').length;
-			var $current = index+1;
-			var $percent = ($current/$total) * 100;
-			$('#validate-wizard').find('.progress-bar').css({width:$percent+'%'});
-			$('#validate-wizard').find('.wizard-status span').html($current+" de "+$total);
-		}
-	});
-
-});
-
 // Obtem os paises no arquivo json
 function paises(chave) {
     var elemento = document.getElementById(chave);
@@ -81,3 +31,5 @@ function paises(chave) {
     xhttp.open("GET", "assets/json/paises.json", true);
     xhttp.send();
 }
+
+
