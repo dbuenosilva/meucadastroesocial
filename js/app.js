5842de8b75e95dd72f1dd9e38ba0632eec5c5c50
allan-costa <allanc.avelar@gmail.com>
2018-07-21 14:32:49 -0300

    tela login

---
+++ js/app.js
@@ -0,0 +1,61 @@
+(function() {
+
+    //Iniciar o firebase
+    const config = {
+        apiKey: "AIzaSyDlYh547Hhw4ezxYn1IFfp6o4njiLJwg1Q",
+        authDomain: "meu-esocial.firebaseapp.com",
+        databaseURL: "https://meu-esocial.firebaseio.com",
+        projectId: "meu-esocial",
+        storageBucket: "",
+        messagingSenderId: "319863127297"
+    };
+
+    firebase.initializeApp(config);
+
+    //Pegar elementos
+    const txtEmail = document.getElementById('txtEmail');
+    const txtSenha = document.getElementById('txtSenha');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnSignUp = document.getElementById('btnSignUp');
+    const btnLogout = document.getElementById('btnLogout');
+
+    // Add Login Event
+    btnLogin.addEventListener('click', e => {
+        //Get email and pass
+        const email = txtEmail.value;
+        const senha = txtSenha.value;
+        const auth = firebase.auth();
+
+        // Sign in
+        const promisse = auth.signInWithEmailAndPassword(email, senha);
+        promisse.catch(e => console.log(e.message));
+    });
+
+    btnSignUp.addEventListener('click', e => {
+        //Get email and pass
+        const email = txtEmail.value;
+        const senha = txtSenha.value;
+        const auth = firebase.auth();
+
+        // Sign in
+        const promisse = auth.createUserWithEmailAndPassword(email, senha);
+        promisse
+            .catch(e => console.log(e.message));
+    });
+
+    btnLogout.addEventListener('click', e => {
+        firebase.auth().signOut();
+    });
+
+    // Add a realtime listener to auth
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            console.log(firebaseUser)
+            btnLogout.classList.remove('d-none');
+        } else {
+            console.log("Usuario não logado")
+            btnLogout.classList.add('d-none');
+        }
+    });
+
+}());
