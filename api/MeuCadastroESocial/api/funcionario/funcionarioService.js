8c3e56e62f9c69e66d7cc513c19f7a0dce0a055b
Diego Bueno <diego@bueno.eti.br>
2018-07-21 10:58:16 -0300

    Reorganizacao.

--- api/MeuCadastroESocial/api/funcionario/funcionarioService.js
+++
@@ -1,6 +0,0 @@
-const Funcionario = require('./funcionario')
-
-Funcionario.methods(['get','post','put','delete'])
-Funcionario.updateOptions({new: true, runValidators: false})
-
-module.exports = Funcionario
--- api/MeuCadastroESocial/api/funcionario/qualificacaoService.js
+++
@@ -1,5 +0,0 @@
-const Qualificacao = require('./qualificacao')
-
-Qualificacao.post('/qualificacao/', Qualificacao.validaNaApiJava);
-
-module.exports = Qualificacao
